eef335cd561ba116cf7aba5f2f35f6859fd394d2
louannecostes <louanne.costes@gmail.com>
2023-04-28 14:52:57 +0200

    ajout dernieres éléments, ajout liens pour dlwd cv

--- PROJETPHOTOCOM_V2/parcours/parcours_lou.js
+++ PROJETPHOTOCOM_V2/parcours/parcours_lou.js
@@ -5,15 +5,24 @@ export const parcours_lou = {
         {
             start_pos: {x:0 , y:0 ,z:0, r:0},
             move_to  : {x:21.5 , y:0 ,z:63.5, r:3.66},
-            img      : "ErreurParcours.png",
+            img      : "ParcoursUniversitaire.png",
             audio    : "1.ogg",
             date     : "01/01/2001",
-            title    : "Mon parcours et mon erreur de parcours",
-            text     : "En effet j'ai fait une erreur de parcours lors du choix du bac au lycée.J'ai choisi la voie royale pensant que cette étape allait être franchissable comme les années précédentes. J'ai ei tord et j'ai préféré changer de voie vers un bac STI2D qui se rapproche le plus d'un bac Scientifique. Après cela, j'ai eu mon bac avec mention et j'ai pu aller DUT et poursuivre mon chemin jusqu'ici."
+            title    : "Mon parcours",
+            text     : "J'ai obtenu mon baccalauréat STI2D qui est une filière technologique axée sur les sciences et technologies de l'industrie et du développement durable. J'ai ensuite poursuivi avec un DUT informatique, cette formation est axée sur la programmation, les bases de données, les réseaux informatiques, ainsi que sur la conception et le développement de logiciels.Puis j'ai continué avec une licence 3 MIASHS, cette formation m'a permis d'acquérir des compétences en modélisation et analyse de données, ainsi que des connaissances en sciences sociales. Enfin, je suis actuellement en master ICE-LD, spécialisé dans l'ingénierie des connaissances et la gestion de données."
         },
         {
             start_pos: {x:0 , y:0 ,z:0, r:0},
             move_to  : {x:54 , y:0 ,z:31.5, r:-2.14},
+            img      : "ErreurParcours.png",
+            audio    : "1.ogg",
+            date     : "01/01/2001",
+            title    : "Mon erreur de parcours",
+            text     : "En effet j'ai fait une erreur de parcours lors du choix du bac au lycée.J'ai choisi la voie royale pensant que cette étape allait être franchissable comme les années précédentes. J'ai ei tord et j'ai préféré changer de voie vers un bac STI2D qui se rapproche le plus d'un bac Scientifique. Après cela, j'ai eu mon bac avec mention et j'ai pu aller DUT et poursuivre mon chemin jusqu'ici."
+        },
+        {
+            start_pos: {x:0 , y:0 ,z:0, r:0},
+            move_to  : {x:67 , y:0 ,z:-3, r:-1.74},
             img      : "autoportrait.png",
             audio    : "1.ogg",
             date     : "01/01/2001",
@@ -22,16 +31,26 @@ export const parcours_lou = {
         },
         {
             start_pos: {x:0 , y:0 ,z:0, r:0},
-            move_to  : {x:67 , y:0 ,z:-3, r:-1.74},
+            move_to  : {x:65 , y:0 ,z:-47, r:-1.275},
             img      : "guitar.png",
             audio    : "1.ogg",
             date     : "01/01/2001",
             title    : "Mes centres d'intérêt 2",
             text     : "La musique me passionne autant que le design. Je joue de différents instruments comme par exemple ici la guitare ou j'ai pu prendre des cours durant plusieurs année. Ensuite d'années en années j'ai commencé à m'intéresser à la basse, au piano et au ukulélé en autodidacte et tout ça juste pour le fun !"
         },
+
         {
             start_pos: {x:0 , y:0 ,z:0, r:0},
-            move_to  : {x:65 , y:0 ,z:-47, r:-1.275},
+            move_to  : {x:-32.5 , y:0 ,z:-109.5, r:0.17},
+            img      : "astronaute.png",
+            audio    : "1.ogg",
+            date     : "01/01/2001",
+            title    : "Mes centres d'intérêt 3",
+            text     : "Je suis passionnée par le ciel, souvent il m'arrive de scruter le ciel pour voir la forme des nuages, il m'arrive de suivre les tempêtes de près sur les radars météo. La nuit souvent quand le ciel est dégagé je sort ma lunette astronomique pour scruter la lune et j'apprends à reconnaitre les constellations."
+        },
+        {
+            start_pos: {x:0 , y:0 ,z:0, r:0},
+            move_to  : {x:42.5 , y:0 ,z:-86, r:-0.8},
             img      : "photography.png",
             audio    : "1.ogg",
             date     : "01/01/2001",
@@ -40,21 +59,21 @@ export const parcours_lou = {
         },
         {
             start_pos: {x:0 , y:0 ,z:0, r:0},
-            move_to  : {x:42.5 , y:0 ,z:-86, r:-0.8},
+            move_to  : {x:8 , y:0 ,z:-107, r:-0.33},
             img      : "europe.png",
             audio    : "1.ogg",
             date     : "01/01/2001",
             title    : "Ma projection à 5 ans premier scénario",
-            text     : "Dans 5 ans, j'aimerais travailler pour quelque chose qui a du sens. J'ai apprit récemment qu'il était possible de travailler pour l'Europe en tant qu'ingénieur en Informatique. Pourquoi ne pas essayer ?"
+            text     : "Dans 5 ans, j'aimerais travailler pour quelque chose qui a du sens. J'ai apprit récemment qu'il était possible de travailler pour l'Europe en tant qu'ingénieur en Informatique. Pourquoi envisager une carrière dans l'union européenne ? Dans les institutions de l’UE, on peut travailler sur les grands dossiers qui nous concernent tous: le changement climatique, la migration, la sûreté et la sécurité, le commerce et les communications mobiles, pour n’en citer que quelques-uns. On a également la possibilité de travailler dans les agences de l’UE établies dans toute l'Europe et dans les délégations de l’UE dans le monde entier."
         },
         {
             start_pos: {x:0 , y:0 ,z:0, r:0},
-            move_to  : {x:8 , y:0 ,z:-107, r:-0.33},
+            move_to  : {x:-32.5 , y:0 ,z:-109.5, r:0.17},
             img      : "esanté.png",
             audio    : "1.ogg",
             date     : "01/01/2001",
             title    : "Ma projection à 5 ans deuxième scénario",
-            text     : "Dans un second temps, j'ai toujours été attirée par la santé. Encore plus lors de la période Covid, je me suis rendue compte qu'il y avait encore d'énormes progrès à faire dans ce domaine là. Ayant une mère travaillant à l'Agence Régionale de Santé du tarn-et-garonne, je suis de près les nouveaux plans santé qui sont mit en place pour les années à venir. J'ai envie de participer."
+            text     : "Dans un second temps, j'ai toujours été attirée par la santé. Encore plus lors de la période Covid, je me suis rendue compte qu'il y avait encore d'énormes progrès à faire dans ce domaine là. L’accélération de l’innovation dans les soins de santé s’appuie sur des percées technologiques majeures qui ont un impact direct sur le parcours de soins et les systèmes de santé, et redéfinissent les relations entre professionnels de santé et patients. Ayant une mère travaillant à l'Agence Régionale de Santé du Tarn-et-Garonne, je suis de près les nouveaux plans santé qui sont mit en place pour les années à venir. J'ai envie de participer."
         },
         {
             start_pos: {x:0 , y:0 ,z:0, r:0},
@@ -68,47 +87,29 @@ export const parcours_lou = {
         {
             start_pos: {x:0 , y:0 ,z:0, r:0},
             move_to  : {x:-32.5 , y:0 ,z:-109.5, r:0.17},
-            img      : "_1090145_mini.png",
+            img      : "AttentesSocioprof.png",
             audio    : "1.ogg",
             date     : "01/01/2001",
-            title    : "Mes formations complémentaires",
-            text     : "écrire un paragraphe long comme la mort ici, et sen fote"
+            title    : "Attentes socio-professionnelles",
+            text     : "Je souhaite avoir une vie professionelle sans stress, une bonne paie, des avantages et surtout ne jamais cesser d'apprendre !"
         },
         {
             start_pos: {x:0 , y:0 ,z:0, r:0},
-            move_to  : {x:-32.5 , y:0 ,z:-109.5, r:0.17},
-            img      : "_1090145_mini.png",
+            move_to  : {x:-32.5 , y:0 ,z:109.5, r:0.17},
+            img      : "fonte.png",
             audio    : "1.ogg",
             date     : "01/01/2001",
-            title    : "un titre de chris 2",
-            text     : "écrire un paragraphe long comme la mort ici, et sen fote"
+            title    : "Bonus",
+            text     : "En bonus je vous présente une de mes oeuvres les plus réussies. Ici une planète en verre avec un liquide à l'intérieur entrain de fondre sur notre planète. Cette illustration dénonce le réchauffement climatique vue de l'espace. "
         },
         {
             start_pos: {x:0 , y:0 ,z:0, r:0},
-            move_to  : {x:-32.5 , y:0 ,z:-109.5, r:0.17},
-            img      : "_1090145_mini.png",
+            move_to  : {x:-32.5 , y:0 ,z:109.5, r:0.17},
+            img      : "pontVieuxPortait.png",
             audio    : "1.ogg",
             date     : "01/01/2001",
-            title    : "un titre de chris 2",
-            text     : "écrire un paragraphe long comme la mort ici, et sen fote"
-        },
-        {
-            start_pos: {x:0 , y:0 ,z:0, r:0},
-            move_to  : {x:-32.5 , y:0 ,z:-109.5, r:0.17},
-            img      : "_1090145_mini.png",
-            audio    : "1.ogg",
-            date     : "01/01/2001",
-            title    : "un titre de chris 2",
-            text     : "écrire un paragraphe long comme la mort ici, et sen fote"
-        },
-        {
-            start_pos: {x:0 , y:0 ,z:0, r:0},
-            move_to  : {x:-32.5 , y:0 ,z:-109.5, r:0.17},
-            img      : "_1090145_mini.png",
-            audio    : "1.ogg",
-            date     : "01/01/2001",
-            title    : "un titre de chris 2",
-            text     : "écrire un paragraphe long comme la mort ici, et sen fote"
+            title    : "Bonus",
+            text     : "Je vous laisse apprécier la vue du pont de sapiac de Montauban qui donne sur le pont Vieux de cette charmante ville."
         }
     ]
 }
